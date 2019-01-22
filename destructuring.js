9cd44cd66d6a62c89a6c877491fb5995e53a9a23
frenchie048 <37281870+frenchie048@users.noreply.github.com>
2019-01-21 23:18:48 -0700

    finished destructuring file problems

--- destructuring.js
+++ destructuring.js
@@ -22,7 +22,7 @@ var carDetails = {
 */
 
 //Code Here
-
+let { color, make, model, year } = carDetails;
 
 
 ////////// PROBLEM 2 //////////
@@ -33,9 +33,9 @@ var carDetails = {
   The property names are firstName, lastName, and title.
 */
 
-function greeting( obj ) {
+function greeting(obj) {
   //Code Here
-  
+  let { firstName, lastName, title } = obj;
   // Do not edit the code below.
   return 'Hello, ' + title + ' ' + firstName + ' ' + lastName + '!';
   // Do not edit the code above.
@@ -54,7 +54,10 @@ function greeting( obj ) {
 */
 
 //Code Here
-
+var totalPopulation = (obj) => {
+  let { utah, california, texas, arizona } = obj;
+  return utah + california + texas + arizona;
+};
 
 
 ////////// PROBLEM 4 //////////
@@ -68,6 +71,12 @@ function greeting( obj ) {
 */
 
 //Code Here
+function ingredients(obj) {
+  let { carb, fat, protein } = obj;
+  newArray = [];
+  newArray.push(carb, fat, protein);
+  return newArray;
+}
 
 
 
@@ -86,7 +95,15 @@ function greeting( obj ) {
 */
 
 //Code Here
-
+function largeNumbers({ first, second, third }) {
+  if (first < second && first < third) {
+    return first
+  } else if (second < first && second < third) {
+    return second
+  } else if (third < first && third < second) {
+    return third
+  }
+};
 
 
 ////////// PROBLEM 6 //////////
@@ -98,5 +115,12 @@ function greeting( obj ) {
 */
 
 //Code Here
-
-
+function numberGroups({ a, b, c }) {
+  if (a.length > b.length && a.length > c.length) {
+    return a
+  } else if (b.length > a.length && b.length > c.length) {
+    return b
+  } else if (c.length > a.length && c.length > b.length) {
+    return c
+  }
+}
